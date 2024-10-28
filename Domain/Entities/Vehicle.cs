303a675a951c0a4125acfaea9a77704ad5cdbb29
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-10-28 15:18:24 +0200

    Add MongoDB support and new Vehicle class
    - Added MongoDB.Bson package reference to Domain.csproj.
    - Introduced Vehicle class in Domain.Entities with BSON attributes.
    - Updated MyGarage.API.csproj with Application and Infrastructure references.

--- Domain/Entities/Vehicle.cs
+++ Domain/Entities/Vehicle.cs
@@ -1,6 +1,23 @@
 ﻿namespace Domain.Entities;
 
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 public class Vehicle
 {
+    [BsonId]
+    [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
 
+    [BsonElement("make"), BsonRepresentation(BsonType.String)]
+    public required string Make { get; set; }
+
+    [BsonElement("model"), BsonRepresentation(BsonType.String)]
+    public required string Model { get; set; }
+
+    [BsonElement("vin"), BsonRepresentation(BsonType.String)]
+    public required string Vin { get; set; }
+
+    [BsonElement("licensePlate"), BsonRepresentation(BsonType.String)]
+    public string? LicensePlate { get; set; }
 }
