54c9f5ffa646e71677e05f5a8a5902f03f9c6bc5
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-10-28 15:52:25 +0200

    Add VehicleController and update project structure
    - Introduced VehicleController to handle vehicle-related API requests.
    - Cleaned up MyGarage.API.csproj by removing an unnecessary <ItemGroup>.
    - Renamed connection string key in appsettings.Development.json for clarity.

---
+++ MyGarage.API/Controllers/VehicleController.cs
@@ -0,0 +1,25 @@
+﻿namespace MyGarage.API.Controllers
+{
+    using Domain.Entities;
+    using Infrastructure.Data;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using MongoDB.Driver;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IMongoCollection<Vehicle>? _vehicles;
+        public VehicleController(MongoDbService mongoDbService)
+        {
+            _vehicles = mongoDbService.Database?.GetCollection<Vehicle>("vehicle");
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Vehicle>> Get()
+        {
+            return await _vehicles.Find(FilterDefinition<Vehicle>.Empty).ToListAsync();
+        }
+    }
+}
