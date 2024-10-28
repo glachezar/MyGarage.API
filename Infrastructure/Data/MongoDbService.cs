e80b7433aa7a3a93ca4de43d8952d53b029cc319
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-10-28 14:34:15 +0200

    Add MongoDbService and update configuration settings
    Introduced a new `MongoDbService` class in the `Infrastructure.Data` namespace to manage MongoDB connections using a configuration-based connection string. Updated `Infrastructure.csproj` with necessary package references (`Microsoft.Extensions.Configuration.Abstractions`, `MongoDB.Driver`, `MongoDB.Driver.Core`). Modified `appsettings.Development.json` to change the connection string key from `DefaultConnection` to `DbConnection`.

---
+++ Infrastructure/Data/MongoDbService.cs
@@ -0,0 +1,21 @@
+﻿namespace Infrastructure.Data;
+
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+public class MongoDbService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IMongoDatabase? _database;
+    public MongoDbService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        var connectionString = _configuration.GetConnectionString("DbConnection");
+        var mongoUrl = MongoUrl.Create(connectionString);
+        var client = new MongoClient(mongoUrl);
+        _database = client.GetDatabase(mongoUrl.DatabaseName);
+    }
+
+    public IMongoDatabase? Database => _database;
+}
+
